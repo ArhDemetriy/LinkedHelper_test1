8ea55d3690830dce3e4daa06dbbc6af4d0df9884
ArhDemetriy <ArhDemetriy@GMail.com>
2023-01-16 23:46:34 +0800

    dev

--- src/run.ts
+++ src/run.ts
@@ -78,8 +78,94 @@ class Executor {
     }
 }
 
+class Iterator {
+    constructor(queue: AsyncIterable<ITask>) {
+        this.iterator = queue[Symbol.asyncIterator]()
+    }
+    private iterator: AsyncIterator<ITask, any, undefined> | null
+    protected async getNextIteratorItem(): Promise<ITask | null> {
+        if (this.iterator == null)
+            return null
+
+        const next = await this.iterator.next()
+        if (next.done)
+            return this.iterator = null
+
+        return next.value
+    }
+}
+class Tasks extends Iterator{
+    private readonly maxAwaitedTasks = 10
+    constructor(queue: AsyncIterable<ITask>, private readonly maxThreads: number) {
+        super(queue)
+    }
+    private readonly queue: Map<TTaskId, ITask[]> = new Map()
+    private tempTask?: ITask | null
+    private setTempTask(task: ITask) {
+        this.tempTask = task
+    }
+    private getTempTask() {
+        const temp = this.tempTask
+        this.tempTask = null
+        return temp
+    }
+    private fillingQueue = false
+    private async fillQueue() {
+        if (this.fillingQueue)
+            return
+        if (this.maxThreads && this.queue.size >= this.maxThreads)
+            return
+        this.fillingQueue = true
+
+        while (true) {
+            const task = this.getTempTask() ?? await new Promise<ITask | null>((t, c) => setTimeout(() => this.getNextIteratorItem().then(t, c)))
+            if (task == null)
+                break
+
+            if (!this.queue.has(task.targetId)) {
+                if (this.maxThreads && this.queue.size >= this.maxThreads) {
+                    this.setTempTask(task)
+                    break
+                }
+                this.queue.set(task.targetId, [])
+            }
+
+            const tasks = this.queue.get(task.targetId)!
+            tasks.push(task)
+            if (tasks.length >= this.maxAwaitedTasks) // стараемся не набирать слишком много тасков в очередь
+                if (this.maxThreads && this.queue.size >= this.maxThreads) // однако в приоритете занимаем все потоки
+                    break
+        }
+
+        this.fillingQueue = false
+    }
+}
+
 export default async function run(executor: IExecutor, queue: AsyncIterable<ITask>, maxThreads = 0) {
     maxThreads = Math.max(0, maxThreads);
+
+    const threads: Set<TTaskId> = new Set()
+    const tasks: Map<TTaskId, ITask[]> = new Map()
+    async function addNewTask(tasks: ITask[]) { }
+    async function taskRunner(tasks: ITask[], executor: IExecutor) {
+        while (true) {
+            if (!tasks.length)
+                await addNewTask(tasks)
+            if (!tasks.length)
+                break
+
+            await executor.executeTask(tasks.shift()!)
+        }
+    }
+
+    function runNewTask(task: ITask) {
+        if (threads.has(task.targetId)) {
+
+        }
+
+
+
+    }
 
     for await (const task of queue) {
 
