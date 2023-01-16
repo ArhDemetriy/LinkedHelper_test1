df7a345c52603ca5c72d70f9d5ab56e2d2f42249
ArhDemetriy <ArhDemetriy@GMail.com>
2023-01-16 21:14:03 +0800

    init Executor

--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,46 @@
 import { IExecutor } from './Executor';
 import ITask from './Task';
 
+interface IQueue{
+    id: ITask['targetId']
+    tasks: ITask['action'][]
+}
+
+class Executor {
+    public static run(executor: IExecutor, queue: AsyncIterable<ITask>, maxThreads = 0) {
+        this.iterator = queue[Symbol.asyncIterator]()
+        this.executor = executor
+    }
+
+    // executor
+    private static executor?: Pick<IExecutor, 'executeTask'>
+    private static async exec(task: ITask): Promise<void> {
+        if (this.executor != null)
+            return this.executor.executeTask(task)
+    }
+    // iterator
+    private static iterator?: AsyncIterator<ITask, any, undefined> | null
+    private static async getNextQueueItem(): Promise<ITask | null> {
+        if (this.iterator == null)
+            return null
+
+        const next = await this.iterator.next()
+        if (next.done)
+            return this.iterator = null
+
+        return next.value
+    }
+}
+
 export default async function run(executor: IExecutor, queue: AsyncIterable<ITask>, maxThreads = 0) {
     maxThreads = Math.max(0, maxThreads);
-    /**
-     * Код надо писать сюда
-     * Тут что-то вызываем в правильном порядке executor.executeTask для тасков из очереди queue
-     */
+
+    for await (const task of queue) {
+
+        executor.executeTask(task)
+            .then(() => {
+
+                task.targetId
+            })
+    }
 }
